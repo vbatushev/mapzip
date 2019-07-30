d3e83a75df60009e76304e907f22aad267bc8553
Vitaly Batushev <vitaly@batushev.info>
2019-07-30 11:11:41 +0300

    Версия 1.3

--- mapzip.go
+++ mapzip.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
@@ -14,11 +15,12 @@ import (
 )
 
 var (
-	version      = "1.2"
+	version      = "1.3"
 	appVersion   = "mapzip " + version
 	startPath    = "./"
 	prefixFolder = ""
 	targetPath   string
+	commonFolder = "D:\\vital\\Documents\\Drofa\\Projects\\drofa.map\\Карты\\common"
 )
 
 func init() {
@@ -26,6 +28,7 @@ func init() {
 	vers := flag.Bool("version", false, "version")
 	flag.StringVar(&prefixFolder, "prefix", "", "Префикс искомой папки")
 	flag.StringVar(&startPath, "start", "./", "Стартовый путь")
+	flag.StringVar(&commonFolder, "target", commonFolder, "Путь для копирования")
 	flag.Parse()
 
 	if *version || *vers {
@@ -51,8 +54,31 @@ func main() {
 	for _, f := range files {
 		if f.IsDir() && strings.HasPrefix(f.Name(), prefixFolder) {
 			zipFolder(f.Name())
+			copyFolder(f.Name())
 		}
 	}
+}
+
+func copyFolder(fldName string) error {
+	err := removeFolderContents(commonFolder)
+	if err != nil {
+		return err
+	}
+	absPath, _ := filepath.Abs(path.Join(targetPath, fldName))
+	ff, err := ioutil.ReadDir(absPath)
+	if err == nil {
+		for _, f := range ff {
+			fname := path.Join(absPath, f.Name())
+			if checkPath(f, fname) {
+				if f.IsDir() {
+					copyDir(fname, filepath.Join(commonFolder, f.Name()))
+				} else {
+					copyFile(fname, filepath.Join(commonFolder, f.Name()))
+				}
+			}
+		}
+	}
+	return nil
 }
 
 func zipFolder(fldName string) error {
@@ -105,4 +131,82 @@ func checkPath(f os.FileInfo, p string) bool {
 		return false
 	}
 	return true
+}
+
+// Копирование файла
+func copyFile(source string, dest string) (err error) {
+	sourcefile, err := os.Open(source)
+	if err != nil {
+		return err
+	}
+	defer sourcefile.Close()
+
+	destfile, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+	defer destfile.Close()
+
+	_, err = io.Copy(destfile, sourcefile)
+	if err == nil {
+		sourceinfo, err := os.Stat(source)
+		if err != nil {
+			err = os.Chmod(dest, sourceinfo.Mode())
+		}
+	}
+	return
+}
+
+// Копирование директории
+func copyDir(source string, dest string) (err error) {
+	// Получаем сведения об исходной директории
+	sourceinfo, err := os.Stat(source)
+	if err != nil {
+		return err
+	}
+	// Создаем новую директорию
+	err = os.MkdirAll(dest, sourceinfo.Mode())
+	if err != nil {
+		return err
+	}
+	directory, _ := os.Open(source)
+	objects, err := directory.Readdir(-1)
+	for _, obj := range objects {
+		sourcefilepointer := source + "/" + obj.Name()
+		destinationfilepointer := dest + "/" + obj.Name()
+		if obj.IsDir() {
+			// Создаем рекурсивно папки в глубину
+			err = copyDir(sourcefilepointer, destinationfilepointer)
+			if err != nil {
+				log.Println(err)
+			}
+		} else {
+			// Копируем файлы
+			err = copyFile(sourcefilepointer, destinationfilepointer)
+			if err != nil {
+				log.Println(err)
+			}
+		}
+	}
+	return
+}
+
+// Удаление всех файлов в папке
+func removeFolderContents(path string) error {
+	d, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer d.Close()
+	names, err := d.Readdirnames(-1)
+	if err != nil {
+		return err
+	}
+	for _, name := range names {
+		err = os.RemoveAll(filepath.Join(path, name))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
